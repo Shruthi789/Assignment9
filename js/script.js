a9e40da6bb540bd67e524b5eefc4f62cec1374a4
Shruthi Sridhar <shruthisridhar96@gmail.com>
2021-09-23 20:32:01 +0530

    Fox Image Assignment

--- js/script.js
+++ js/script.js
@@ -1,60 +1,45 @@
-//Submitting data
-let tableInfo=[];
-function submitData(event){
-    event.preventDefault();
-   let textInputs=document.querySelectorAll('input[type="text"]');
-   let textAreaInput=document.querySelector('textarea');
-   let radioInputs=document.querySelectorAll('input[type="radio"]');
-   let checkedInputs=document.querySelectorAll('input[type="checkbox"]');
-   let obj={
-       FirstName:textInputs[0].value,
-       LastName:textInputs[1].value,
-       Address:textAreaInput.value,
-       Pincode:textInputs[2].value,
-       Gender:radioInputs[0].checked?radioInputs[0].value:radioInputs[1].checked?radioInputs[1].value:'',
-       FoodChoices: (FoodChoices(checkedInputs).length<2)?confirm('Please select at least two food options'):FoodChoices(checkedInputs),
-       State:textInputs[3].value,
-       Country:textInputs[4].value
-    };
-   if(tableInfo.findIndex((element)=>JSON.stringify(element)===JSON.stringify(obj))!==-1){
-   alert('Data already exists');
-   }
-   else if(obj.FoodChoices!==true&&obj.FoodChoices!==false) {
-    tableInfo.push(obj);
-    addTableRow(obj);
-    document.querySelector('form').reset();
-   }
-}
-//Getting the food choices
-function FoodChoices(inputs){
-let foodChoiceArr=[];
-inputs.forEach((element)=>{
-    if(element.checked){
-        foodChoiceArr.push(element.value);
-    }
-});
-return foodChoiceArr;
-}
-//Adding the Table Row
-function addTableRow(rowObj){
- let tableRow=document.createElement('tr');
- for(var key in rowObj){
-     let tableData=document.createElement('td');
-     if(key==='FoodChoices'){
-        let List=document.createElement('ul');
-        List.style="display:inline;padding:0px;list-style-type:none;";
-        rowObj[key].forEach((element)=>{
-           let item=document.createElement('li');
-           item.appendChild(document.createTextNode(element));
-           List.appendChild(item);
-        });
-        tableData.appendChild(List);
-     }
-     else{
-     tableData.appendChild(document.createTextNode(rowObj[key]));
-     }
-     tableRow.appendChild(tableData);
- }
- document.getElementById('data-table').appendChild(tableRow);    
 
+ /*Getting the table*/
+ let DataTable=document.querySelector('.data-table');
+ /*Getting a Fox Image */
+ async function getFoxImage(){
+   try{
+   const response=await fetch('https://randomfox.ca/floof/');
+   const data=await response.json();
+   return data;
+   }
+   catch(error){
+     document.querySelector('.error-message').append(`${error}`);
+   }
+ }
+ 
+ /*Add a Fox Image */
+ document.querySelector('.displayButton').addEventListener('click',addFoxImage);
+ async function addFoxImage(){
+   try{
+    let responseData=await getFoxImage();
+    DataTable.insertAdjacentHTML('beforeend',`<tr><td>${getRowIndex()}</td><td><img src="${responseData.image}" alt="Fox Image" height="130" width="130"></td><td><button class="updateButton" onclick="updateFoxImage(this.parentNode.parentNode)">Update Image</button></td><td><button class="deleteButton" onclick="deleteFoxImage(this.parentNode.parentNode)">Delete Image</button></td></tr>`);
+   }
+   catch(error){
+    document.querySelector('.error-message').append(`  ${error}`);
+   }
+  }
+ /*Update a Fox Image */
+ async function updateFoxImage(row){
+   try{
+  let responseData=await getFoxImage();
+   row.childNodes[1].firstChild.src=responseData.image;
+   }
+   catch(error){
+    document.querySelector('.error-message').append(`  ${error}`);
+   }
+ }
+ /*Delete a Fox Image */
+function deleteFoxImage(row){
+   DataTable.deleteRow(row.rowIndex);
+   if(DataTable.rows.length>=2){
+   DataTable.rows[getRowIndex()-1].firstChild.innerHTML=getRowIndex()-1;
+   }
 }
+/*Update the row Index value on the table*/
+let getRowIndex=()=>DataTable.rows.length;
